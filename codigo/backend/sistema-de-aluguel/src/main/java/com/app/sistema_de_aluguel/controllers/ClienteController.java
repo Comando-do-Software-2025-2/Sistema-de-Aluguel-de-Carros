c7b1d707f4150116bc54c74084fdedaf6576caf0
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-09-18 14:06:14 -0300

    feat: cliente controller

--- codigo/backend/sistema-de-aluguel/src/main/java/com/app/sistema_de_aluguel/controllers/ClienteController.java
+++ codigo/backend/sistema-de-aluguel/src/main/java/com/app/sistema_de_aluguel/controllers/ClienteController.java
@@ -1,7 +1,45 @@
 package com.app.sistema_de_aluguel.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.app.sistema_de_aluguel.models.Cliente;
+import com.app.sistema_de_aluguel.services.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/clientes")
+@RequiredArgsConstructor
 public class ClienteController {
+    private final ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> getAll() {
+        return ResponseEntity.ok(clienteService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getById(@PathVariable Long id) {
+        return clienteService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
+        return ResponseEntity.ok(clienteService.create(cliente));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<Cliente>> update(@PathVariable Long id, @RequestBody Cliente clienteAtualizado) {
+        return ResponseEntity.ok(clienteService.update(id, clienteAtualizado));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
