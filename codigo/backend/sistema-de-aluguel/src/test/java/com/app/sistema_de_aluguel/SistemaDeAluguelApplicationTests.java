71b106f2232f50fe46138b20a65a6fcaeda37504
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-09-18 13:48:56 -0300

    feat: spring starter configured

---
+++ codigo/backend/sistema-de-aluguel/src/test/java/com/app/sistema_de_aluguel/SistemaDeAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sistema_de_aluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
