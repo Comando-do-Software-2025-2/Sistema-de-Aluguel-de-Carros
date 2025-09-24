6db90ef25670b13948790333d24ad995367a738f
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-09-24 20:21:42 -0300

    feat: constructor for user models

--- codigo/backend/sistema-de-aluguel/src/main/java/com/app/sistema_de_aluguel/models/Usuarios/Agente.java
+++ codigo/backend/sistema-de-aluguel/src/main/java/com/app/sistema_de_aluguel/models/Usuarios/Agente.java
@@ -1,18 +1,26 @@
 package com.app.sistema_de_aluguel.models.Usuarios;
 
+import com.app.sistema_de_aluguel.models.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Set;
+
 
 @Entity
 @Table
-@NoArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Agente extends Usuario {
     @Column(unique = true)
     private String cnpj;
+
+    public Agente(String senha, String email, String nome, String endereco, Set<Role> permissoes, String cnpj) {
+        super(senha, email, nome, endereco, permissoes);
+        this.cnpj = cnpj;
+    }
 }
--- codigo/backend/sistema-de-aluguel/src/main/java/com/app/sistema_de_aluguel/models/Usuarios/Cliente.java
+++ codigo/backend/sistema-de-aluguel/src/main/java/com/app/sistema_de_aluguel/models/Usuarios/Cliente.java
@@ -1,11 +1,13 @@
 package com.app.sistema_de_aluguel.models.Usuarios;
 
+import com.app.sistema_de_aluguel.models.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table
@@ -23,4 +25,11 @@ public class Cliente extends Usuario {
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Rendimento> rendimentos;
 
+    public Cliente(String senha, String email, String nome, String endereco, Set<Role> permissoes, String rg, String cpf, String profissao, List<Rendimento> rendimentos) {
+        super(senha, email, nome, endereco, permissoes);
+        this.rg = rg;
+        this.cpf = cpf;
+        this.profissao = profissao;
+        this.rendimentos = rendimentos;
+    }
 }
